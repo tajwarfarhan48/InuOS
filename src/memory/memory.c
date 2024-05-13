101ea5f981b53fe8397e29edcbcf1e47963d8eda
Farhan Tajwar Romit <tajwarfarhan48@gmail.com>
2024-05-12 21:11:06 -0500

    Started adding physical memory allocator

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,5 +1,7 @@
 #include "memory/memory.h"
 
+phy_pageinfo phy_pages[PHY_NUMPAGES];
+
 /*
  * Initialize a region of memory to a given value.
  *
@@ -17,4 +19,76 @@ void* memset(void* ptr, int c, size_t size)
         p[i] = (char) c; 
 
     return ptr;
+}
+
+static inline uint32_t phy_to_pfn(uint32_t phy_addr) {
+    return phy_addr >> PHY_PAGESIZE_BITS;
+}
+
+static inline uint32_t pfn_to_phy(uint32_t pfn) {
+    return pfn << PHY_PAGESIZE_BITS;
+}
+
+uint32_t kmalloc(uint32_t size) {
+    uint32_t num_alloc_pages, i, x, alloc_pfn = 0;
+
+    num_alloc_pages = size / (PHY_PAGESIZE) + ((size % (PHY_NUMPAGES) == 0) ? 0 : (PHY_PAGESIZE));
+    
+    // Handle 0-byte allocations
+    if (num_alloc_pages == 0) goto fail;
+
+    for (i = 0; i < PHY_NUMPAGES; i++) {
+        // Empty page found
+        if (!(phy_pages[i] | PHY_ALLOCATED)) {
+            if (x == 0) alloc_pfn = i; // This is the starting address of the allocation
+            x++;
+            if (x == num_alloc_pages) break; // We have found a large enough contiguous block of memory
+        }
+
+        else x = 0; // Large enough contiguous block not found. Restart search
+    }
+
+    if (x == num_alloc_pages) {
+        phy_pages[alloc_pfn] = phy_pages[alloc_pfn] | PHY_BLOCKSTART; // Mark page as start of allocated block
+
+        // Mark just-allocated pages as already allocated 
+        for (i = alloc_pfn; i < alloc_pfn + x; i++)
+            phy_pages[i] = phy_pages[i] | PHY_ALLOCATED;
+
+        return pfn_to_phy(alloc_pfn);
+    }
+
+fail:
+    return (uint32_t) -1;
+}
+
+uint32_t kzalloc(uint32_t size) {
+    uint32_t alloc_addr;
+
+    if ((alloc_addr = kmalloc(size)) == -1) return -1;
+
+    memset((void *) alloc_addr, 0, size);
+
+    return alloc_addr;
+}
+
+void kree(uint32_t addr) {
+    uint32_t pfn;
+
+    if (addr % PHY_PAGESIZE != 0) {
+        panic();
+    }
+
+    pfn = phy_to_pfn(addr);
+
+    if (!(phy_pages[pfn] & PHY_BLOCKSTART)) {
+        panic();
+    }
+
+    phy_pages[pfn] &= ~PHY_BLOCKSTART;
+
+    while (!(phy_pages[pfn] & PHY_BLOCKSTART) && (phy_pages[pfn] & PHY_ALLOCATED)) {
+        phy_pages[pfn] &= ~PHY_ALLOCATED;
+        pfn++;
+    }
 }
--- src/memory/memory.h
+++ src/memory/memory.h
@@ -1,8 +1,24 @@
-#ifndef MEMORY_C
-#define MEMORY_C
+#ifndef MEMORY_H
+#define MEMORY_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+// Physical page allocation bitmasks
+#define PHY_ALLOCATED 0x1
+#define PHY_BLOCKSTART 0X2
+
+// Constants describing the size of physical memory
+#define PHY_PAGESIZE_BITS 12
+#define PHY_PAGESIZE 1 << PHY_PAGESIZE_BITS
+#define PHY_NUMPAGES 1 << (32 - PHY_PAGESIZE_BITS)
+
+// Structure representing information about a page
+typedef uint8_t phy_pageinfo;
+extern phy_pageinfo phy_pages[PHY_NUMPAGES];
 
 void* memset(void* ptr, int c, size_t size);
+uint32_t kmalloc(uint32_t size);
+uint32_t kzalloc(uint32_t size);
 
 #endif
